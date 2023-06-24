b16323454f0f061302123666847f512b89fe4173
Immortal <ak2061435@gmail.com>
2023-06-24 10:36:10 +0530

    new item add

--- lib/screens/grocery_items.dart
+++ lib/screens/grocery_items.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping/data/categories.dart';
 
 class GroceryItemsScreen extends StatefulWidget {
   const GroceryItemsScreen({super.key});
@@ -14,9 +15,56 @@ class _GroceryItemsScreen extends State<GroceryItemsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Grocery items'),
+        title: const Text('New Item'),
       ),
-      body: const Text('Grocery Items here'),
+      body: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Form(
+              child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(label: Text('Name')),
+                validator: (value) {
+                  return 'Testing';
+                },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration:
+                          const InputDecoration(label: Text('quantity')),
+                      initialValue: '1',
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: DropdownButtonFormField(items: [
+                      for (final category in categories.entries)
+                        DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 23,
+                                  height: 23,
+                                  color: category.value.color,
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Text(category.value.title)
+                              ],
+                            ))
+                    ], onChanged: (value) {}),
+                  )
+                ],
+              )
+            ],
+          ))),
     );
   }
 }
